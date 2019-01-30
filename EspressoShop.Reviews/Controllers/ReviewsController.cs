a34c41ef75708cfdbee249fbf7826463557e4850
Hossam Barakat <hossam.eldeen@gmail.com>
2019-01-31 09:09:41 +1100

    customize response of reviews service based on SERVICE_VERSION setting

--- EspressoShop.Reviews/Controllers/ReviewsController.cs
+++ EspressoShop.Reviews/Controllers/ReviewsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace EspressoShop.Reviews.Controllers
 {
@@ -9,6 +10,7 @@ namespace EspressoShop.Reviews.Controllers
     [ApiController]
     public class ReviewsController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
         private Review[] reviews = new Review[] {
                 new Review
                 {
@@ -17,7 +19,8 @@ namespace EspressoShop.Reviews.Controllers
                     Headline = "Cool Coffee",
                     ReviewText = "asd",
                     CreationDate = DateTime.Today,
-                    ReviewerName = "Jon"
+                    ReviewerName = "Jon",
+                    Stars = 1
 
                 },
                 new Review
@@ -27,14 +30,28 @@ namespace EspressoShop.Reviews.Controllers
                     Headline = "Flat white",
                     ReviewText = "cool coffee",
                     CreationDate = DateTime.Today.AddDays(-20),
-                    ReviewerName = "Paul"
+                    ReviewerName = "Paul",
+                    Stars = 5
                 }
             };
 
+        public ReviewsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         [HttpGet("{productId}")]
         public ActionResult<IEnumerable<Review>> Get(int productId)
         {
-            return reviews.Where(x => x.ProductId == productId).ToArray() ;
+            var serviceVersion = _configuration.GetValue<string>("SERVICE_VERSION");
+            if(serviceVersion == "v1")
+            {
+                return reviews.Select(x => { x.Stars = null; return x; }).Where(x => x.ProductId == productId).ToArray();
+            }
+            else
+            {
+                return reviews.Where(x => x.ProductId == productId).ToArray();
+            }
+            
         }
     }
     public class Review
@@ -45,5 +62,6 @@ namespace EspressoShop.Reviews.Controllers
         public DateTime CreationDate { get; set; }
         public int ProductId { get; set; }
         public string ReviewerName { get; set; }
+        public int? Stars { get; set; }
     }
 }
